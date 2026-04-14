Fix syntax error in feature scaling for categorical data

Attrition.py
@@ -35,7 +35,7 @@ if st.button("Click here to get the Prediction"):
     for i in x_num:
         scaled_features.append(scaler.transform(i))
     for i in x_cat:
-        scaled_features.append(le.transform(i)
+        scaled_features.append(le.transform(i))
                            
     arr = np.array(scaled_features).reshape(1,12)
     prediction = model.predict(arr)