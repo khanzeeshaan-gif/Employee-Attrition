e941329eec1a14fbc89e6c7b309f1d9027ea08d4
Zeeshaan Khan <khanzeeshaan4@gmail.com>
2026-04-15 09:35:50 +0530

    Fix syntax error in feature scaling for prediction

--- Attrition.py
+++ Attrition.py
@@ -35,7 +35,7 @@ if st.button("Click here to get the Prediction"):
     scaled_features=[]
     scaled_features.append(scaler.transform(x_num))
     for i in x_cat:
-        scaled_features.append(le.transform([i])
+        scaled_features.append(le.transform([i]))
                            
     arr = np.array(scaled_features).reshape(1,12)
     prediction = model.predict(arr)
